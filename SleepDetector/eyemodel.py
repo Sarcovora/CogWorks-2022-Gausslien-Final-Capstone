1e074fe20d18770df38177664e1a14b6937b6bcb
Alan W <awangc31s@gmail.com>
2022-08-02 14:29:24 -0400

    ffff

---
+++ SleepDetector/eyemodel.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class EyeModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 5, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(5, 7, 3)
+        self.fc1 = nn.Linear(112, 2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.fc1(x)
+        return x
--- SleepDetector/main.py
+++ SleepDetector/main.py
@@ -1,7 +1,10 @@
-from eye_img import eye_img
 from camera import take_picture
 import numpy as np
 import skimage.io as io
+import torch
+
+from eye_img import eye_img
+from eyemodel import EyeModel
 
 def user_interface():
     while True:
@@ -16,3 +19,24 @@ def user_interface():
         else:
             print("Invalid input. Try again. ")
         eye_img(pic)
+
+if (not torch.cuda.is_available()):
+    print("NOT USING GPU")
+    model = torch.load("model.pb", map_location=torch.device('cpu'))
+else:
+    print("USING GPU")
+    model = torch.load("model.pb", map_location=torch.device('cuda'))
+
+left, right = user_interface()
+
+left = torch.tensor(left).reshape(1,1,24,24).float()/255
+right = torch.tensor(right).reshape(1,1,24,24).float()/255
+
+left = torch.argmax(model(left)).item()
+right = torch.argmax(model(right)).item()
+
+def printer(eyeopen):
+    return "open" if eyeopen else "closed"
+
+print("Left eye "+printer(left))
+print("Right eye "+printer(right))
