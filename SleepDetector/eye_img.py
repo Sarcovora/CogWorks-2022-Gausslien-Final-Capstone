ce36e5ef263e99bd157d3c25b14b5b9eaa6ab6f3
Alan W <awangc31s@gmail.com>
2022-08-02 13:47:31 -0400

    fixed import name

--- SleepDetector/eye_img.py
+++ SleepDetector/eye_img.py
@@ -2,7 +2,7 @@ import numpy as np
 from facenet_models import FacenetModel
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-import cv2
+import cv2 as cv
 
 def find_eyes(image_data, *, box_threshold=0.97, prop_const=0.13, disp=False):
     """
